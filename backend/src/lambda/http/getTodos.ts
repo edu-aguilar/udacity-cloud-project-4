cc28b1a8a326c994f104cd9f3c647f3010f9de89
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-10 13:56:36 +0200

    fix(chore): temporarily change config to allow step by step development

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,6 +2,10 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   // TODO: Get all TODO items for a current user
+// }
+
+export const handler: APIGatewayProxyHandler = (event: APIGatewayProxyEvent): void => {
   // TODO: Get all TODO items for a current user
 }
