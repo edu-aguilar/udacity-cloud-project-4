7861014ce6c984b8744da5a77316c5f5402f737d
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-11 13:07:13 +0200

    feat(auth): create auth0rizer and added to create and get todos

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,16 +12,14 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-0t584hj4.eu.auth0.com/.well-known/jwks.json'
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+
   logger.info('Authorizing a user', event.authorizationToken)
   try {
-    const jwtToken = await verifyToken(event.authorizationToken)
+    const jwtToken: JwtPayload = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
-
     return {
       principalId: jwtToken.sub,
       policyDocument: {
@@ -54,14 +52,9 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+const fetchJWKS = async jwksUrl => {
+  const result = await Axios.get(jwksUrl)
+  return result.data.keys
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +67,45 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+const getJWKSSigningKey = (jwks, kid) => {
+
+  const getJWKSSigningKeys = jwks => {
+
+    const certToPEM = cert => {
+      let pem = cert.match( /.{1,64}/g ).join( '\n' );
+      pem = `-----BEGIN CERTIFICATE-----\n${ cert }\n-----END CERTIFICATE-----\n`;
+      return pem;
+    }
+
+    return jwks
+    .filter(
+      ( key ) =>
+        key.use === 'sig' && 
+        key.kty === 'RSA' &&
+        key.kid &&
+        ( ( key.x5c && key.x5c.length ) || ( key.n && key.e ) ) // Has useful public keys
+    )
+    .map( ( key ) => ( { kid: key.kid, nbf: key.nbf, publicKey: certToPEM( key.x5c[ 0 ] ) } ) );
+  }
+
+  return getJWKSSigningKeys(jwks).find( key => key.kid === kid );
+}
+
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
+
+  const jwks = await fetchJWKS(jwksUrl)
+  const token = getToken(authHeader)
+  const decodedToken: Jwt = decode(token, { complete: true }) as Jwt
+
+  const { header } = decodedToken;
+  if ( !header || header.alg !== 'RS256' ) {
+    throw new Error( 'Token is not RS256 encoded' );
+  }
+
+  const key = getJWKSSigningKey(jwks, header.kid)
+  const actualKey = key.publicKey || key.rsaPublicKey;
+
+  return verify(token, actualKey, { algorithms: ['RS256'] }) as JwtPayload
 }
