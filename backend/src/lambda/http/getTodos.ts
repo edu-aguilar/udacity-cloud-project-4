b33af82c04f6945613d774ca32514d85bdf16c22
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-11 10:35:53 +0200

    feat(getTodos): get todos from hardcoded user

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,11 +1,46 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   // TODO: Get all TODO items for a current user
-// }
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const todosIdIndex = process.env.TODOS_ID_INDEX
 
-export const handler: APIGatewayProxyHandler = (event: APIGatewayProxyEvent): void => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+
+  // sacar userID del JWT de la cabecera auth
+  const userId = '12345'
+
+  const result = await docClient.query({
+    TableName : todosTable,
+    IndexName : todosIdIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': userId
+      }
+  }).promise()
+
+  console.log('results: ', result.Items);
+  
+
+  if (result.Count !== 0) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(result.Items)
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
+
 }
