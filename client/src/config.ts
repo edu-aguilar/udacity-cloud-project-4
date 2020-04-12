2eaa5799ffe6fc753f1407dbae6136dc8fbb56a3
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-12 11:54:59 +0200

    chore(config): update apiId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'k0m8dti7v5'
+const apiId = '47gheayy1i'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
