c9a9efad5ae0fb0e8d7d37f65cc64f71716d8d6b
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-12 12:14:58 +0200

    feat(deleteItem): delete todo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,31 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { DynamoDB } from 'aws-sdk'
+import { getUserId } from '../utils'
+
+const docClient = new DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const userId: string = getUserId(event)
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  await docClient.delete({
+    TableName: todosTable,
+    Key: {
+      "userId": userId,
+      "todoId": todoId
+    }
+  }).promise()
+
+  console.log('Record deleted successfully');
+  
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
 }
